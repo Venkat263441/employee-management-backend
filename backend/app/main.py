76d2c4c20b48a42e183afa5601989690f13d378d
karthik <venkat263441@gmail.com>
2026-08-20 17:31:31 +0530

    Fix CORS configuration

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,6 +4,7 @@ from sqlalchemy import text
 from .database import Base, engine
 from . import models
 from .routers import employee,department,user, auth,attendance,leave,dashboard
+from fastapi.middleware.cors import CORSMiddleware
 
 
 Base.metadata.create_all(bind=engine)
@@ -15,6 +16,17 @@ app = FastAPI(
     version="1.0.0"
 )
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://127.0.0.1:5500",
+        "http://localhost:5500",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(
     employee.router
